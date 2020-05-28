d191186b4333be70cb6c1286b155d774e4ff6744
windows 10 <windows 10@DESKTOP-UPFA2M2>
2020-05-29 02:28:30 +0545

    Program 1

---
+++ src/internshipp/Program1.java
@@ -0,0 +1,24 @@
+package internshipp;
+import java.util.Scanner;
+
+public class Program1 {
+
+public static void main(String[] args) {
+    
+        Scanner sc =  new Scanner(System.in);
+        
+        String name;
+        
+        System.out.println("Please Enter Your Name---");
+        
+        //name = sc.next();//Single String
+          name = sc.nextLine();//multi sentence 
+        
+        //System.out.println("The Name is:  " + name);
+        System.out.println("The Name is--------------------:  " + name);
+    }
+    
+} 
+    
+    
+
