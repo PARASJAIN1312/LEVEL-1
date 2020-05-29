6e298ab269668b416f960a230cd456c991511f9d
windows 10 <windows 10@DESKTOP-UPFA2M2>
2020-05-29 20:34:15 +0545

    Program14

---
+++ src/internshipp/Program14.java
@@ -0,0 +1,16 @@
+package internshipp;
+import java.util.Scanner;
+
+public class Program14 {
+ 
+    public static void main(String[] args)
+    {
+        String s1 = "15 august is independence day";
+        String s2 = s1.replace("15", "26");
+        String s3 = s2.replace("august", "january");
+        String s4 = s3.replace("independence", "republic");
+        System.out.println(s4);
+    }
+}
+   
+
