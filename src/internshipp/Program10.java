4580e01d0c4b9b083ca3f1ce7b06b72c3a9c518d
windows 10 <windows 10@DESKTOP-UPFA2M2>
2020-05-29 18:21:19 +0545

    Program10

---
+++ src/internshipp/Program10.java
@@ -0,0 +1,15 @@
+package internshipp;
+
+
+public class Program10 {
+    public static void main(String[] args)
+    {
+        String str = "PARAS JAIN";
+        String name,lp,lp1;
+       
+        lp1 = str.substring(0,3);
+        lp = str.substring(3);
+        System.out.println(lp1 + "   " + lp);
+    }
+  
+}   
