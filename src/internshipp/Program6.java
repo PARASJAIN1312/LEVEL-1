2e8dd144d485a5e6695b39a0ee1b21fbb381e161
windows 10 <windows 10@DESKTOP-UPFA2M2>
2020-05-29 17:53:36 +0545

    Program 6

---
+++ src/internshipp/Program6.java
@@ -0,0 +1,15 @@
+package internshipp;
+import java.util.Scanner;
+
+ 
+public class Program6 {
+
+    public static void main(String[] args)
+    {           
+        String s1 = "PIKACHHU";
+        String s2 = "pikachhu";
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equalsIgnoreCase(s2));
+    }
+}
+  
