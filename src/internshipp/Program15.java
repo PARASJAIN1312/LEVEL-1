3e5092dc73f43c6e57de2c4b9afb1aee258346ed
windows 10 <windows 10@DESKTOP-UPFA2M2>
2020-05-29 20:40:57 +0545

    Program15

---
+++ src/internshipp/Program15.java
@@ -0,0 +1,12 @@
+package internshipp;
+public class Program15 {
+
+    
+    public static void main(String[] args)
+    {
+        String s1 = "    hi    ";
+        System.out.println(s1.trim());
+    }
+
+}
+ 
